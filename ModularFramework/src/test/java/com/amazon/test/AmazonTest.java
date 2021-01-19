5958afc9167381e33714135270f5fc22dabb6f5c
emildam <77644542+emildam@users.noreply.github.com>
2021-01-19 23:25:40 +0000

    Test changes cli

--- ModularFramework/src/test/java/com/amazon/test/AmazonTest.java
+++ ModularFramework/src/test/java/com/amazon/test/AmazonTest.java
@@ -37,7 +37,7 @@ CommonDriver cmnDriver;
 	
 	@Test
 	public void searchProduct() throws Exception{
-		String product = "iPhone";
+		String product = "Test";
 		String category = "Electronics";
 		
 		homepage.searchProduct(product, category);
